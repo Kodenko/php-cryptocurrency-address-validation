82142e66a6bcd0a282c2c5fbadefa595f7b0fbe5
Den Korolev <you@example.com>
2025-02-20 08:37:08 +0300

    fix:ton validate

--- src/Drivers/Base64Crc16Driver.php
+++ src/Drivers/Base64Crc16Driver.php
@@ -24,6 +24,10 @@ class Base64Crc16Driver extends AbstractDriver
 
             $decodedData = $base64Decode->urlDecode($address);
 
+            if(!$this->validateLength($decodedData)) {
+                return false;
+            }
+
             $addressWithoutCrc = substr($decodedData, 0, 34);
 
             $providedCrc = bin2hex(substr($decodedData, 34, 2));
@@ -36,20 +40,26 @@ class Base64Crc16Driver extends AbstractDriver
         }
     }
 
+    private function validateLength(string $address): bool
+    {
+        return strlen($address) === 36;
+    }
+
     private function calculateCrc(string $data): string
     {
-        $crc = 0x0000;
+        $crc = 0;
         $bytes = str_split($data);
 
         foreach ($bytes as $byte) {
-            $crc ^= ord($byte);
+            $crc ^= ord($byte) << 8;
 
             for ($j = 0; $j < 8; $j++) {
-                if ($crc & 0x0001) {
-                    $crc = (($crc >> 1) ^ 0x8408) & 0xFFFF;
+                if ($crc & 0x8000) {
+                    $crc = (($crc << 1) ^ 0x1021) & 0xFFFF;
                 } else {
-                    $crc >>= 1;
+                    $crc <<= 1;
                 }
+                $crc &= 0xFFFF;
             }
         }
 
@@ -58,6 +68,7 @@ class Base64Crc16Driver extends AbstractDriver
 
     private function getPattern(string $address): false|int
     {
+
         return preg_match('/^(UQ|EQ|kQ|0Q|Uf|EF)[A-Za-z0-9\-_]{46}$/', $address);
     }
 }
